c92a81a0c32bff41d10e1bda6af23a5e2293c331
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-05-23 10:44:12 +0900

    feat:add element-ui

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,21 @@ import router from "./router"
 import store from "./store"
 import "./registerServiceWorker"
 
+import * as Sentry from "@sentry/browser"
+import * as Integrations from "@sentry/integrations"
+import "./plugins/element.js"
+
 Vue.config.productionTip = false
+
+Sentry.init({
+  dsn: "https://453e0f6436ac4fb8bfe0819cc4ac2c98@sentry.io/1466014",
+  integrations: [
+    new Integrations.Vue({
+      Vue,
+      attachProps: true
+    })
+  ]
+})
 
 new Vue({
   router,
