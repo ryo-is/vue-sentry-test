4eae975313761a336bf2c34142b4e95de44b1d2e
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-05-23 11:00:05 +0900

    feat:start verification

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,9 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  #app
+    router-view
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld.vue"
-
-export default {
-  name: "app",
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+<template lang="pug">
+  .home
+    h1.title Hello Vue.js!!
+    el-button(@click="test()") Click Here!!
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator"
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+@Component({})
+export default class Home extends Vue {
+  public test() {
+    (document.querySelector(".hoge") as Element).append("test")
+  }
+}
 </script>
